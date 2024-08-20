3e447893fd7fb1b0a3eef01c064855b118f65af2
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-08-19 19:45:47 -0500

    Create Persona.java

---
+++ Personas/Persona.java
@@ -0,0 +1,16 @@
+package Personas;
+
+public class Persona {
+  String nombre; // Atributo que identifica el nombre de una persona
+  String apellidos; /* Atributo que identifica los apellidos de una
+  persona */
+  String teléfono; // Atributo que identifica el teléfono de una persona
+  String dirección; /* Atributo que identifica la dirección de una
+  
+  public Persona(String nombre, String apellidos, String teléfono, String dirección) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.teléfono = teléfono;
+    this.dirección = dirección;
+  }
+}
